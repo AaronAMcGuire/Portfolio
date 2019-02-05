ebeb1927b3299b2901465a0fa6fafa9ee978e812
Aaron McGuire <aaron.mcguire0@gmail.com>
2019-02-05 13:04:48 +0000

    added Blog Card

--- src/components/BlogBlock.vue
+++ src/components/BlogBlock.vue
@@ -6,32 +6,68 @@
          <h2 class='blog-introduction__title'>Read Through Aarons Latest Thoughts </h2>
          <h3 class='blog-introduction__subtitle'>Browse the most recent of Aaron's posts</h3>
      </div>
+     <div class='blog-card-container'>
+     <BlogCard Author='https://picsum.photos/50/50?random' CoverImg="https://picsum.photos/380/230?random" Category="Laravel" Title="From WordPress to Artisan - The Beginning"/>
+     <BlogCard Author='https://picsum.photos/50/50?random' CoverImg="https://picsum.photos/380/230?random" Category="WordPress" Title="Self Initiated Project: Powered By WordPress"/>
+     <BlogCard Author='https://picsum.photos/50/50?random' CoverImg="https://picsum.photos/380/230?random" Category="CSS" Title="Working With The Future: CSS Grid Layouts"/>
+     </div>
+      <a href='' class='blog-card__more' title=''>View All Posts <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-chevron-right"><polyline points="9 18 15 12 9 6"/></svg></a>
  </div>   
 
 </section>
 </template>
 
 <script>
+import BlogCard from './BlogCard';
 
+export default {
+    name:'BlogBlock',
+    components:{
+        BlogCard
+    }
+}
 </script>
 
 
 
 <style scoped>
 
-.blog-introduction{
-    margin-top:
-}
 .blog-introduction__title{
     text-transform:uppercase;
     color:#5319e1;
     font-size:14px;
+    letter-spacing: 1px;
     margin-bottom:10px;
 }
 .blog-introduction__subtitle{
     font-size:35px;
     font-weight:400;
     max-width:400px;
+    
+}
+.blog-card-container{
+    display:flex;
+    justify-content: space-around;
+    margin:80px 0 40px 0;
+}
+
+.blog-card__more{
+    padding:15px 25px;
+    color:rgb(255,255,255);
+    background-color:#5319e1;
+    border-radius:8px;
+    text-decoration:none;
+    margin-right:45px;
+    float:right;
+    width:180px;
+    position:relative;
+} 
+
+.blog-card__more svg{
+    position:absolute;
+    top:50%;
+    transform:translateY(-50%);
+    right:10px;
     
 }
 </style>
---
+++ src/components/BlogCard.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="blog-card">
+    <div class='blog-card-image'><img :src="CoverImg" alt=''></div>
+    <div class='blog-card-content'>
+        <span class='blog-card-content__author'><img :src="Author" alt=''></span>
+        <span class='blog-card-content__category'>{{Category}}</span>
+        <a class='blog-card-content__link' href='' title=''><h2 class='blog-card-content__title'>{{Title}}</h2></a>
+
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "BlogCard",
+  props: {
+    Title: String,
+    Description: String,
+    Category: String,
+    Author: String,
+    CoverImg: String,
+  }
+};
+</script>
+<style scoped>
+.blog-card{
+border-radius:5px;
+    flex-basis:27%;
+    transition: .3s ease;
+}
+.blog-card:hover{
+    box-shadow: 0 20px 20px 5px rgba(33,55,87,0.05);
+    transition: .3s ease;
+}
+.blog-card-content{
+    padding: 40px;
+    background:rgb(255,255,255);
+    min-height: 120px;
+    position: relative;
+}
+.blog-card-image{
+    background:rgb(230,230,230);
+    height: 230px;
+   
+}
+.blog-card-image img{
+    display:block;
+    width:100%;
+    height:100%;
+    object-fit: cover;
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
+}
+.blog-card-content__author{
+    width:50px;
+    height:50px;
+    border-radius:100rem;
+    position:absolute;
+    top:-25px;
+    right:25px;
+    background-color:rgb(200,200,200);
+    overflow: hidden;
+}
+.blog-card-content__author img{
+    width:100%;
+    height:100%;
+    object-fit: cover;
+    display:block;
+}
+.blog-card-content__title{
+    color:#162944;
+    font-size:26px;
+    font-weight:400;
+    line-height:35px;
+}
+.blog-card-content__category{
+    color:#5319e1;
+    margin-bottom:15px;
+    display:block;
+    font-size:14px;
+    text-transform:uppercase;
+    font-family:'Roboto-Bold';
+    letter-spacing: 1px;
+
+}
+.blog-card-content__link{
+    text-decoration: none;
+}
+
+</style>
